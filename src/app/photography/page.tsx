0a0ba4710564607a6c3a65289ed3d261fb40dddd
Jacob Bailes <contact@jbailes.com>
2025-01-20 20:52:09 -0500

    pages

---
+++ src/app/photography/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PhotographyPage: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-yellow-100'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold text-yellow-600'>🚧 Under Construction 🚧</h1>
+        <p className='mt-4 text-lg text-gray-700'>We're working hard to bring you something special!</p>
+        <p className='mt-2 text-sm text-gray-500'>Please check back soon!</p>
+      </div>
+    </div>
+  )
+}
+
+export default PhotographyPage
