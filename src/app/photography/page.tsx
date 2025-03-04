a0dbc0674b3121f378bc63b110977912af1fe190
Jacob Bailes <jakebailes76@yahoo.com>
2025-03-04 13:58:14 -0500

    test

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -7,6 +7,19 @@ const PhotographyPage: React.FC = () => {
         <h1 className='text-4xl font-bold text-yellow-600'>🚧 Under Construction 🚧</h1>
         <p className='mt-4 text-lg text-gray-700'>We're working hard to bring you something special!</p>
         <p className='mt-2 text-sm text-gray-500'>Please check back soon!</p>
+
+        {/* YouTube Embed Video */}
+        <div className='mt-6'>
+          <iframe
+            width='560'
+            height='315'
+            src='https://www.youtube.com/embed/xvFZjo5PgG0'
+            title='YouTube video player'
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </div>
   )
