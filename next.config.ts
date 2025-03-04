a086d73894d1af633d4fb6d1281fc5b6054748e2
Jacob Bailes <jakebailes76@yahoo.com>
2025-03-04 10:14:25 -0500

    test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next'
 
-const isGithubPages = process.env.NODE_ENV === 'production' // Adjust for production builds
-
-const nextConfig: NextConfig = {
-  basePath: isGithubPages ? '/miranda' : '',
-  assetPrefix: isGithubPages ? '/miranda' : '',
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
