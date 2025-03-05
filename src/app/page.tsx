61b3368372e3d316ef5c75e7c46c13d16ae6173f
Jacob Bailes <jakebailes76@yahoo.com>
2025-03-05 09:29:54 -0500

    mobile: home screen fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,15 @@ export default function Home() {
     >
       <div className='grid grid-cols-1 md:grid-cols-2 '>
         {/* Text Section */}
-        <div className='sm:flex sm:flex-col justify-center text-left sm:mb-32 mb-6 '>
+        <div className='sm:flex sm:flex-col justify-center text-left sm:mb-32 mb-2 '>
           <div className='sm:mb-0 mb-4 '>
             <h1 className='text-gray-800 font-bold text-center sm:text-massive '>MIRANDA</h1>
-            <h1 className='text-gray-400 text-center' style={{ letterSpacing: '2em' }}>
-              FILM MAKER
-            </h1>
+            <h1 className='text-gray-400 text-center sm:tracking-[2em] tracking-[.5em]'>FILM MAKER</h1>
+
             <h1 className='text-gray-800 font-bold text-center sm:text-massive '>GRAVES</h1>
           </div>
           <h2></h2>
-          <h1 className='text-center text-2xl text-gray-400'>
+          <h1 className='text-center text-2xl sm:block hidden text-gray-400'>
             <Link href='/director'>
               <span className='hover:text-orange-500 cursor-pointer'>DIRECTOR</span>
             </Link>
@@ -44,9 +43,33 @@ export default function Home() {
         </div>
 
         {/* Image Section */}
-        <div className='flex flex-col text-center items-center justify-center mb-32'>
+        <div className='flex flex-col text-center items-center justify-center sm:mb-32 mb-4'>
           <img className='rounded-md' src='./me.jpg' alt='desc' width={500} height={500} />
         </div>
+
+        <h1 className='text-center sm:hidden text-lg text-gray-400'>
+          <Link href='/director'>
+            <span className='hover:text-orange-500 cursor-pointer'>DIRECTOR</span>
+          </Link>
+
+          {' | '}
+          <Link href='/writing'>
+            <span className='hover:text-orange-500 cursor-pointer'>WRITER</span>
+          </Link>
+          {' | '}
+          <Link href='/creative'>
+            <span className='hover:text-orange-500 cursor-pointer'>CREATIVE ARTS</span>
+          </Link>
+          {' | '}
+          <Link href='/photography'>
+            <span className='hover:text-orange-500 cursor-pointer'>PHOTOGRAPHER</span>
+          </Link>
+          {' | '}
+
+          <Link href='/productiondesign'>
+            <span className='hover:text-orange-500 cursor-pointer'>PROD. DESIGN</span>
+          </Link>
+        </h1>
       </div>
     </div>
   )
